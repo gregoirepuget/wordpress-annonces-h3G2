3906c50fbdb76d1adab3f9003a6dfcfbcff45b33
gregoirepuget <gregoirepuget@gmail.com>
2016-10-13 15:02:15 +0200

    ajout wp_query front

--- wp-content/themes/annonces/front-page.php
+++ wp-content/themes/annonces/front-page.php
@@ -1,7 +1,36 @@
 <?php get_header(); //appel du template header.php  ?>
 
+<section class="container" id="listeAnnonces">
 
-Page d'accueil du site
+  <?php
+  
+  $args=array( 'post_type' => 'annonce' );
 
+  // The Query
+  $the_query = new WP_Query( $args );
+
+  // The Loop
+  if ( $the_query->have_posts() ) {
+    
+    while ( $the_query->have_posts() ) 
+    {
+        $the_query->the_post();
+    ?>
+        <article class="col-md-4">
+          <h2><?php the_title(); ?></h2>
+          <span class="prix">Prix : <?php the_field('prix'); ?>€</span>
+        </article>
+    <?php      
+    }
+  
+    /* Restore original Post Data */
+    wp_reset_postdata();
+    } else {
+    // no posts found
+    }
+?>
+  
+  
+</section>
 <?php get_footer(); ?>
 
