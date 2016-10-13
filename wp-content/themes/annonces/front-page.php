5a97be0f89bbbd5ca5765110aa2574b986c6282f
gregoirepuget <gregoirepuget@gmail.com>
2016-10-13 14:49:59 +0200

    debut front

---
+++ wp-content/themes/annonces/front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); //appel du template header.php  ?>
+
+
+Page d'accueil du site
+
+<?php get_footer(); ?>
+
--- wp-content/themes/annonces/functions.php
+++ wp-content/themes/annonces/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+add_action( 'after_setup_theme', 'thumbnails_theme_support' );
+function thumbnails_theme_support(){
+    add_theme_support( 'post-thumbnails' );
+}
+
+
 add_action('init', 'create_custom_post_type_annonces');
 
 function create_custom_post_type_annonces(){
@@ -35,7 +41,11 @@ function create_custom_post_type_annonces(){
         'has_archive'         => false,
         'query_var'           => true,
         'can_export'          => true,
-        'rewrite'             => array( 'slug' => 'annonce' )
+        'rewrite'             => array( 'slug' => 'annonce' ),
+      'capabilities' => array(
+  'edit_post'          => 'edit_annonce', 
+  'create_posts'       => 'edit_annonces', 
+),
     );
     register_post_type( 'annonce', $args );
 }
@@ -52,7 +62,60 @@ function create_custom_taxonomy(){
     'categorie-annonce',
     array('annonce'),
      $args ); 
+  
+  $args2 = array(
+          'label' => 'Mots-clés annonce',
+          'rewrite' => array( 'slug' => 'mots-cles-annonce' ),
+          'hierarchical' => false,
+      );
+  register_taxonomy( 
+    'mots-cles-annonce',
+    array('annonce'),
+     $args2 ); 
 }
+
+
+add_action('after_switch_theme', 'create_new_role');
+function create_new_role(){
+  add_role(
+      'internaute',
+      'Internaute',
+      array(
+          'read'         => true,  // true allows this capability
+          'edit_annonce' => true,
+          'edit_annonces' => true,
+         // Use false to explicitly deny
+      )
+  );
+  
+    $role = get_role( 'administrator');
+    $role->add_cap('edit_annonce');
+    $role->add_cap('edit_annonces');
+}
+
+function ajout_scripts() {
+
+// enregistrement d'un nouveau script
+wp_register_script('bootstrap_script', get_template_directory_uri() . '/scripts/bootstrap.min.js', array('jquery'),'1.0', true);
+
+// appel du script dans la page
+wp_enqueue_script('bootstrap_script');
+
+// enregistrement d'un nouveau style
+wp_register_style( 'bootstrap_style', get_template_directory_uri() . '/styles/bootstrap.min.css' );
+
+// appel du style dans la page
+wp_enqueue_style( 'bootstrap_style' );
+  
+// enregistrement d'un nouveau style
+wp_register_style( 'main_style', get_template_directory_uri() . '/styles/main.css' );
+
+// appel du style dans la page
+wp_enqueue_style( 'main_style' );
+
+}
+add_action( 'wp_enqueue_scripts', 'ajout_scripts' );
+
 
 
 
