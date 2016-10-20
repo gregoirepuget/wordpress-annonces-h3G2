2e12621741ab5edc037b46a3a06e804a05df5c84
gregoirepuget <gregoire@36pixels.fr>
2016-10-20 10:23:15 +0200

    post type slider

--- wp-content/themes/annonces/inc/custom_post_types.php
+++ wp-content/themes/annonces/inc/custom_post_types.php
@@ -26,7 +26,7 @@ function create_custom_post_type_annonces(){
         'public'              => true,
         'show_ui'             => true,
         'show_in_menu'        => true,
-        'menu_position'       => 1,
+        'menu_position'       => -1,
         'menu_icon'           => 'dashicons-megaphone',
         'show_in_nav_menus'   => true,
         'publicly_queryable'  => true,
@@ -49,4 +49,44 @@ function create_custom_post_type_annonces(){
 
     );
     register_post_type( 'annonce', $args );
+}
+
+add_action('init', 'create_custom_post_type_slides');
+
+function create_custom_post_type_slides(){
+  
+    $labels = array(
+        'name'               => 'Slides',
+        'singular_name'      => 'Slide',
+        'all_items'          => 'Toutes les slides',
+        'add_new'            => 'Ajouter une slide',
+        'add_new_item'       => 'Ajouter une nouvelle slide',
+        'edit_item'          => "Modifier la slide",
+        'new_item'           => 'Nouvelle slide',
+        'view_item'          => "Voir la slide",
+        'search_items'       => 'Rechercher une slide',
+        'not_found'          => 'Pas de résultat',
+        'not_found_in_trash' => 'Pas de résultat',
+        'parent_item_colon'  => 'Slide parente:',
+        'menu_name'          => 'Slider',
+    );
+
+    $args = array(
+        'labels'              => $labels,
+        'hierarchical'        => false,
+        'supports'            => array( 'title','thumbnail'),
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'menu_position'       => 0,
+        'menu_icon'           => 'dashicons-megaphone',
+        'show_in_nav_menus'   => true,
+        'publicly_queryable'  => true,
+        'exclude_from_search' => false,
+        'has_archive'         => false,
+        'query_var'           => true,
+        'can_export'          => true,
+        'rewrite'             => array( 'slug' => 'slide' ),
+    );
+    register_post_type( 'slide', $args );
 }
